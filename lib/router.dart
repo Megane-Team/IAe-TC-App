2b68d23832b71758f3aba9979d2c81caac1b83d0
Yrdard <116556664+Yrdoard@users.noreply.github.com>
2024-10-31 09:34:17 +0700

    feat: beranda (#14)
    * 30%
    
    * 100%
    
    * fix: formating

--- lib/router.dart
+++ lib/router.dart
@@ -1,6 +1,8 @@
 import 'package:go_router/go_router.dart';
+import 'package:inventara/page/beranda.dart';
 import 'package:inventara/page/login.dart';
 
-final appRouter = GoRouter(initialLocation: '/login', routes: [
-  GoRoute(path: '/login', builder: (context, state) => const Login()),
+final appRouter = GoRouter(initialLocation: '/Login', routes: [
+  GoRoute(path: '/Login', builder: (context, state) => const Login()),
+  GoRoute(path: '/Beranda', builder: (context, state) => const Beranda()),
 ]);
