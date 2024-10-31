2579a0f571dbc32c617883538715e6f54f4ae94d
Yrdoard <yardoardi@gmail.com>
2024-10-31 09:15:11 +0700

    10%

--- lib/router.dart
+++ lib/router.dart
@@ -1,6 +1,7 @@
 import 'package:go_router/go_router.dart';
+import 'package:inventara/page/gedung.dart';
 import 'package:inventara/page/login.dart';
 
-final appRouter = GoRouter(initialLocation: '/login', routes: [
-  GoRoute(path: '/login', builder: (context, state) => const Login()),
+final appRouter = GoRouter(initialLocation: '/gedung', routes: [
+  GoRoute(path: '/gedung', builder: (context, state) => const Gedung()),
 ]);
