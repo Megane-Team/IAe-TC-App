28c864913ad7c424e9815607b61469919540d995
Yrdoard <yardoardi@gmail.com>
2024-10-31 09:19:55 +0700

    A Revert "10%"
    This reverts commit 2579a0f571dbc32c617883538715e6f54f4ae94d.

--- lib/router.dart
+++ lib/router.dart
@@ -1,7 +1,6 @@
 import 'package:go_router/go_router.dart';
-import 'package:inventara/page/gedung.dart';
 import 'package:inventara/page/login.dart';
 
-final appRouter = GoRouter(initialLocation: '/gedung', routes: [
-  GoRoute(path: '/gedung', builder: (context, state) => const Gedung()),
+final appRouter = GoRouter(initialLocation: '/login', routes: [
+  GoRoute(path: '/login', builder: (context, state) => const Login()),
 ]);
