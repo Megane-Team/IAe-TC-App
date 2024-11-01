cd847d983870589c0404b097eddc20c421bc0027
Yrdoard <yardoardi@gmail.com>
2024-11-01 08:35:54 +0700

    10%

--- lib/page/ruangan.dart
+++ lib/page/ruangan.dart
@@ -15,7 +15,81 @@ class RuanganState extends State<Ruangan>{
       body: SingleChildScrollView(
         child: Stack(
           children: [
-
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 247,
+              decoration: const BoxDecoration(
+                color: Colors.grey,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(50),
+                  bottomRight: Radius.circular(50)
+                )
+              ),
+              child: Image.asset('assets/images/logos/inventara.png'),
+            ),
+            SafeArea(
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 24, right: 24, top: 8),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Container(
+                            width: 40,
+                            decoration: BoxDecoration(boxShadow: [
+                              BoxShadow(
+                                color: Colors.black.withOpacity(0.1),
+                                spreadRadius: 1,
+                                blurRadius: 4,
+                                offset: const Offset(0, 0),
+                              )
+                            ], color: Colors.white, shape: BoxShape.circle),
+                            child: IconButton(
+                              onPressed: () {},
+                              icon: Icon(Icons.navigate_before),
+                            ),
+                          ),
+                          Container(
+                            width: 40,
+                            decoration: BoxDecoration(boxShadow: [
+                              BoxShadow(
+                                color: Colors.black.withOpacity(0.1),
+                                spreadRadius: 1,
+                                blurRadius: 4,
+                                offset: const Offset(0, 0),
+                              )
+                            ], color: Colors.white, shape: BoxShape.circle),
+                            child: IconButton(
+                              onPressed: () {},
+                              icon: Icon(Icons.search),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const Gap(110),
+                      Container(
+                        width: MediaQuery.of(context).size.width,
+                        height: 80,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(16),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black.withOpacity(0.1),
+                              spreadRadius: 1,
+                              blurRadius: 4,
+                              offset: const Offset(0, 0),
+                            )
+                          ]
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              )
+            )
           ],
         ),
       )
