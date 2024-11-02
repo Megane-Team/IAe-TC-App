5d7ac973dc613dde89bc29bab69896c4d49a02d0
Yrdard <116556664+Yrdoard@users.noreply.github.com>
2024-11-02 20:32:58 +0700

    feat: gedung (#15)
    * 10%
    
    * A
    Revert "10%"
    
    This reverts commit 2579a0f571dbc32c617883538715e6f54f4ae94d.
    
    * Revert "A"
    
    This reverts commit 28c864913ad7c424e9815607b61469919540d995.
    
    * feat: Gedung
    
    * fix format
    
    * fix router

--- lib/router.dart
+++ lib/router.dart
@@ -1,8 +1,10 @@
 import 'package:go_router/go_router.dart';
+import 'package:inventara/page/gedung.dart';
 import 'package:inventara/page/beranda.dart';
 import 'package:inventara/page/login.dart';
 
 final appRouter = GoRouter(initialLocation: '/Login', routes: [
   GoRoute(path: '/Login', builder: (context, state) => const Login()),
   GoRoute(path: '/Beranda', builder: (context, state) => const Beranda()),
+  GoRoute(path: '/Gedung', builder: (context, state) => const Gedung()),
 ]);
