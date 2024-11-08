4936efae286b2ec9d91a8ad5ed935c507daac6a6
Yrdoard <yardoardi@gmail.com>
2024-11-08 10:33:01 +0700

    100%

--- lib/router.dart
+++ lib/router.dart
@@ -3,10 +3,12 @@ import 'package:inventara/page/gedung.dart';
 import 'package:inventara/page/beranda.dart';
 import 'package:inventara/page/login.dart';
 import 'package:inventara/page/ruangan.dart';
+import 'package:inventara/page/keranjang.dart';
 
-final appRouter = GoRouter(initialLocation: '/Login', routes: [
+final appRouter = GoRouter(initialLocation: '/Keranjang', routes: [
   GoRoute(path: '/Login', builder: (context, state) => const Login()),
   GoRoute(path: '/Beranda', builder: (context, state) => const Beranda()),
   GoRoute(path: '/Gedung', builder: (context, state) => const Gedung()),
   GoRoute(path: '/Ruangan', builder: (context, state) => const Ruangan()),
+  GoRoute(path: '/Keranjang', builder: (context, state) => const Keranjang()),
 ]);
