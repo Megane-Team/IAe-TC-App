0476eb62b87817d8d26f759e3735de2956e51f85
Yrdard <116556664+Yrdoard@users.noreply.github.com>
2024-11-14 09:26:53 +0700

    feat: riwayat peminjaman (#34)
    * 30%
    
    * 40%
    
    * 80%
    
    * feat: riwayat peminjaman
    
    * feat: riwayat peminjaman
    
    * fix
    
    * fix route

--- lib/router.dart
+++ lib/router.dart
@@ -4,10 +4,12 @@ import 'package:inventara/page/beranda.dart';
 import 'package:inventara/page/login.dart';
 import 'package:inventara/page/ruangan.dart';
 import 'package:inventara/page/keranjang.dart';
+import 'package:inventara/page/riwayat.dart';
 
 final appRouter = GoRouter(initialLocation: '/Login', routes: [
   GoRoute(path: '/Login', builder: (context, state) => const Login()),
   GoRoute(path: '/Beranda', builder: (context, state) => const Beranda()),
+  GoRoute(path: '/Riwayat', builder: (context, state) => const Riwayat()),
   GoRoute(
       path: '/Gedung',
       builder: (context, state) => Gedung(
