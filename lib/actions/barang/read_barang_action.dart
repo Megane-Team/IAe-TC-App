71ee20a391d83020baac8821d5182b27218d9afe
Maulana Yusuf <myalghani@gmail.com>
2024-11-14 14:54:14 +0700

    feat: adding readBarang action

---
+++ lib/actions/barang/read_barang_action.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+
+import 'package:inventara/constants/variables.dart';
+import 'package:inventara/main.dart';
+import 'package:inventara/structures/barang.dart';
+import 'package:inventara/utils/sessions.dart';
+
+Future<List<Barang>> readBarang() async {
+  var token = await Session.getToken();
+
+  final response = await App.api.get(apiBaseURl.resolve('barangs'),
+      headers: { 'authorization': 'Bearer $token' }
+  );
+
+  if (response.statusCode == 200) {
+    final Map<String, dynamic> responseData = jsonDecode(response.body);
+    final List<dynamic> data = responseData['data'];
+    final List<Barang> barangs = data.map((item) => Barang.fromJson(item)).toList();
+
+    return barangs;
+  } else if (response.statusCode == 401) {
+    throw Exception('Unauthorized');
+  } else {
+    throw Exception('Failed to get barang. Is internet connection available?');
+
+  }
+
+}
