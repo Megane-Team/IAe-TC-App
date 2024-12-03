b54911f0bd2e7d2c1c3663dc51728a495270e155
Yrdard <116556664+Yrdoard@users.noreply.github.com>
2024-12-03 10:27:25 +0700

    feat: konfirmasi (#58)
    * hallo
    
    * 100%
    
    * 100%
    
    * fix formating
    
    * fix router

--- lib/router.dart
+++ lib/router.dart
@@ -1,11 +1,13 @@
 import 'package:go_router/go_router.dart';
 import 'package:inventara/page/gedung.dart';
 import 'package:inventara/page/beranda.dart';
+import 'package:inventara/page/konfirmasi_kendaraan.dart';
 import 'package:inventara/page/login.dart';
 import 'package:inventara/page/ruangan.dart';
 import 'package:inventara/page/keranjang.dart';
 import 'package:inventara/page/riwayat.dart';
 import 'package:inventara/page/notifikasi.dart';
+import 'package:inventara/page/konfirmasi_asset.dart';
 import 'package:inventara/page/cari.dart';
 
 final appRouter = GoRouter(initialLocation: '/Login', routes: [
@@ -13,6 +15,9 @@ final appRouter = GoRouter(initialLocation: '/Login', routes: [
   GoRoute(path: '/Beranda', builder: (context, state) => const Beranda()),
   GoRoute(path: '/Riwayat', builder: (context, state) => const Riwayat()),
   GoRoute(path: '/Notifikasi', builder: (context, state) => const Notifikasi()),
+  GoRoute(path: '/KonfA', builder: (context, state) => const Konfirmasiasset()),
+  GoRoute(
+      path: '/KonfK', builder: (context, state) => const Konfirmasikendaraan()),
   GoRoute(path: '/Cari', builder: (context, state) => const Cari()),
   GoRoute(
       path: '/Gedung',
