3610465ca96f7b7784588e811eea9ff99fd8eab2
Yrdoard <yardoardi@gmail.com>
2024-12-10 09:37:50 +0700

    1%

---
+++ lib/page/detail_peminjaman.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:gap/gap.dart';
+import 'package:intl/intl.dart';
+
+class DetailPeminjaman extends StatefulWidget {
+  const DetailPeminjaman({super.key});
+
+  @override
+  State<DetailPeminjaman> createState() => _DetailPeminjamanState();
+}
+
+class _DetailPeminjamanState extends State<DetailPeminjaman> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                width: 40,
+                decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.1),
+                      spreadRadius: 1,
+                      blurRadius: 4,
+                      offset: const Offset(0, 0),
+                    ),
+                  ],
+                  color: Colors.white,
+                  shape: BoxShape.circle,
+                ),
+                child: IconButton(
+                  onPressed: () {
+                    setState(() {
+                      context.pop();
+                    });
+                  },
+                  icon: const Icon(Icons.navigate_before, color: Colors.black),
+                ),
+              ),
+              const Text(
+                'Detail Peminjaman',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+              ),
+              const Gap(40),
+            ],
+          ),
+        ),
+      body: Padding(
+        padding: EdgeInsets.only(right: 24, left: 24, top: 20, bottom: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Text('Status Peminjaman')
+                      // Text('')
+                    ],
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+// List <string string>
--- lib/router.dart
+++ lib/router.dart
@@ -9,12 +9,14 @@ import 'package:inventara/page/riwayat.dart';
 import 'package:inventara/page/notifikasi.dart';
 import 'package:inventara/page/konfirmasi_asset.dart';
 import 'package:inventara/page/cari.dart';
+import 'package:inventara/page/detail_peminjaman.dart';
 
-final appRouter = GoRouter(initialLocation: '/Login', routes: [
+final appRouter = GoRouter(initialLocation: '/DetailP', routes: [
   GoRoute(path: '/Login', builder: (context, state) => const Login()),
   GoRoute(path: '/Beranda', builder: (context, state) => const Beranda()),
   GoRoute(path: '/Riwayat', builder: (context, state) => const Riwayat()),
   GoRoute(path: '/Notifikasi', builder: (context, state) => const Notifikasi()),
+  GoRoute(path: '/DetailP', builder: (context, state) => const DetailPeminjaman()),
   GoRoute(
       path: '/KonfA',
       builder: (context, state) => Konfirmasiasset(
